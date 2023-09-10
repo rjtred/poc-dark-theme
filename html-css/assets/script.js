260f1a99e9ddd51cdac2f28dfef58cbd679a492f
VananJeeva <vananjeeva@VananJeevas-MacBook-Air.local>
2023-09-10 16:03:13 +0530

    init basic html css theme based web

---
+++ html-css/assets/script.js
@@ -0,0 +1,65 @@
+let body = document.querySelector('body')
+let themeSwitcher = document.querySelector('#theme-switcher')
+
+const getnext = () => currentTheme === 'light' ? 'dark' : 'light'
+
+var currentTheme = localStorage.getItem('theme') || 'light'
+var nextTheme = getnext()
+
+const themeLoad = () => {
+  localStorage.setItem('theme', currentTheme)
+  body.className = `theme-${currentTheme}`
+
+  themeSwitcher.innerHTML = `${nextTheme}_mode`
+}
+
+const themeSwitch = () => {
+  nextTheme = currentTheme
+  currentTheme = getnext()
+
+  themeLoad()
+}
+
+themeSwitcher.addEventListener('click', themeSwitch)
+
+const addCardsNavigationHandlers = (navigation) => {
+  let data = navigation.dataset
+  let target = document.querySelector(data.target)
+  let item = target.querySelector(data.item)
+
+  let itemWidth = item.clientWidth
+  let targetWidth = target.clientWidth
+
+  let prevButton = navigation.querySelector('.navigation-prev')
+  let nextButton = navigation.querySelector('.navigation-next')
+
+  prevButton.addEventListener('click', () => {
+    target.scrollBy(
+      {
+        top: 0,
+        left: -itemWidth,
+        behaviour: 'smooth'
+      }
+    )
+  })
+
+  nextButton.addEventListener('click', () => {
+    target.scrollBy(
+      {
+        top: 0,
+        left: itemWidth,
+        behaviour: 'smooth'
+      }
+    ) 
+  })
+}
+
+const initCardsNavigations = () => {
+  const cardsNavigations = document.querySelectorAll('.cards-navigation')
+  cardsNavigations.forEach(addCardsNavigationHandlers)
+}
+
+window.addEventListener('load', () => {
+  themeLoad()
+  initCardsNavigations()
+})
